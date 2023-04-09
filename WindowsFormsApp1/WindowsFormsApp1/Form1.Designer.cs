e87dafa5e6e6003e1a3cfc816398f9deec470f45
Hiran Kavindu Vishmith <97301123+Hirankavindu@users.noreply.github.com>
2023-04-09 11:00:24 -0700

    Add files via upload
    create braille

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.InputTextBox = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.OutputTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // InputTextBox
+            // 
+            this.InputTextBox.Location = new System.Drawing.Point(161, 107);
+            this.InputTextBox.Multiline = true;
+            this.InputTextBox.Name = "InputTextBox";
+            this.InputTextBox.Size = new System.Drawing.Size(264, 65);
+            this.InputTextBox.TabIndex = 0;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(170, 205);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Convert";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(338, 205);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Clear";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // OutputTextBox
+            // 
+            this.OutputTextBox.Location = new System.Drawing.Point(555, 110);
+            this.OutputTextBox.Multiline = true;
+            this.OutputTextBox.Name = "OutputTextBox";
+            this.OutputTextBox.Size = new System.Drawing.Size(195, 95);
+            this.OutputTextBox.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.OutputTextBox);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.InputTextBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox InputTextBox;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox OutputTextBox;
+    }
+}
+
---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+                // Get the input text from the text box
+                string inputText = InputTextBox.Text;
+
+                // Create a dictionary to map regular letters to their corresponding braille letters
+                Dictionary<char, string> brailleMap = new Dictionary<char, string>()
+            {
+                {'a', "⠇"},
+                {'b', "⠃"},
+                {'c', "⠉"},
+                {'d', "⠙"},
+                {'e', "⠑"},
+                {'f', "⠋"},
+                {'g', "⠛"},
+                {'h', "⠓"},
+                {'i', "⠊"},
+                {'j', "⠚"},
+                {'k', "⠅"},
+                {'l', "⠇"},
+                {'m', "⠍"},
+                {'n', "⠝"},
+                {'o', "⠕"},
+                {'p', "⠏"},
+                {'q', "⠟"},
+                {'r', "⠗"},
+                {'s', "⠎"},
+                {'t', "⠞"},
+                {'u', "⠥"},
+                {'v', "⠧"},
+                {'w', "⠺"},
+                {'x', "⠭"},
+                {'y', "⠽"},
+                {'z', "⠵"}
+            };
+
+                // Create a StringBuilder to store the output braille letters
+                StringBuilder outputBuilder = new StringBuilder();
+
+                // Convert each letter of the input text to its corresponding braille letter
+                foreach (char c in inputText)
+                {
+                    // Check if the letter is in the dictionary
+                    if (brailleMap.ContainsKey(c))
+                    {
+                        // If the letter is in the dictionary, add its braille letter to the output
+                        outputBuilder.Append(brailleMap[c]);
+                    }
+                    else
+                    {
+                        // If the letter is not in the dictionary, add a space to the output
+                        outputBuilder.Append(" ");
+                    }
+                }
+
+                // Set the output text to the braille letters
+                OutputTextBox.Text = outputBuilder.ToString();
+            }
+        }
+    }
+
